a662cd0e569156163857ccbcad99d27c476b5f32
booooo <boo2431@gmail.com>
2024-01-31 21:20:22 +0900

    Commit 2024-01-31 6kyu Roman Numerals Encoder

---
+++ codewars/6kyu_RomanNumeralsEncoder.py
@@ -0,0 +1,10 @@
+'''https://www.codewars.com/kata/51b62bf6a9c58071c600001b'''
+def solution(n):
+    dict = {1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+    result = ''
+    for k in dict:
+        qu = int(n/k)
+        result = result + dict[k]*qu
+        n = n%k
+        if n == 0 : break
+    return result
