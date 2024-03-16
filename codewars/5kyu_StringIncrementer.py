58ad59f087127e5548ec0d169dfb170fca14c75b
booooooo <136612690+mychu-lab@users.noreply.github.com>
2024-03-17 00:01:03 +0900

    Commit 2024-03-17 / 5kyu String Incrementer

---
+++ codewars/5kyu_StringIncrementer.py
@@ -0,0 +1,20 @@
+'''https://www.codewars.com/kata/54a91a4883a7de5d7800009c'''
+def increment_string(string):
+    if len(string) > 0 :
+        num = ''
+        n = len(string)
+        for x in reversed(string) :
+            if x.isdigit() :
+                num = x + num
+            else :
+                break;
+            n = n - 1
+    else :
+        return "1"
+    if num == '' :
+        return string + '1'
+    mid = len(string) - n - len(str(int(num) + 1))
+    return string[0:n] + string[n:n+mid]+ str(int(num) + 1)
+
+string = 'foobar001'
+print (increment_string(string))
